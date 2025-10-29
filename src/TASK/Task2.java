7c75025b7ae92a0d3e788727c11f3678dbdcd7be
AREMCEE <30773646+aremcee@users.noreply.github.com>
2025-10-29 10:52:25 +0000

    Tasks

---
+++ src/TASK/Task2.java
@@ -0,0 +1,27 @@
+package TASK;
+
+public class Task2 {
+
+        public static void main (String [] args){
+
+            char H ='H';
+
+            byte three = 3;
+
+            short one = 1;
+
+            int zero = 0;
+
+            float twoPointZero = 2.0f;
+
+            boolean truth = true;
+
+            String output = "" + H + three + one + one + zero + " w" + zero + "r" + one + "d " +
+
+                    twoPointZero +" " + truth;
+
+            System.out.println(output);
+
+        }
+
+    }
--- src/TASK/Task3.java
+++ src/TASK/Task3.java
@@ -1,4 +1,21 @@
 package TASK;
 
-public class Task3 {
-}
+
+    public class Task3 {
+
+        public static void main(String[] args) {
+            String a = "Wow";
+            String b = a;
+            String c = "Not Wow";
+            String d = "Wow!";
+
+            boolean b1 = a == b;                // true
+            boolean b2 = d.equals(b + "!");     // true
+            boolean b3 = !c.equals(a);          // true
+
+            if (b1 && b2 && b3) {
+                System.out.println("Success!");
+            }
+        }
+    }
+
