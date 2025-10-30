c3fbd12c2383444f3a4978436c5132af8527775f
AREMCEE <30773646+aremcee@users.noreply.github.com>
2025-10-30 17:07:42 +0800

    task 112

---
+++ src/TASK/Task_1_1_2.java
@@ -0,0 +1,24 @@
+package TASK;
+
+public class Task_1_1_2 {
+
+        public static void main(String[] args) {
+
+            int result = 1 + 2;
+            System.out.println(result);
+
+            result -= 1;
+            System.out.println(result);
+
+            result *= 2;
+            System.out.println(result);
+
+            result /= 2;
+            System.out.println(result);
+
+            result += 8;
+            result %= 7;
+            System.out.println(result);
+        }
+
+}
